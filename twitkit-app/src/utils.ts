0da82bfcde7d3a97a461666692a5c1ee17bc4458
MisakaHonoka <2361547758@qq.com>
2020-02-06 17:06:59 +0800

    add data check

--- twitkit-app/src/utils.ts
+++ twitkit-app/src/utils.ts
@@ -4,6 +4,9 @@
  * hex: [0-9A-Fa-f]
  */
 export type uuid = string
+export function varifyUUID(str: uuid): boolean {
+    return /[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}/.test(str)
+}
 
 const chars = "0123456789abcdef"
 function randstr(count: number = 1) {
@@ -20,6 +23,9 @@ export function genUuid(): uuid {
  * GUID: \d+"-"(0\d|1[0-2])"-"([0-2]\d|3[0-1])"T"([0-1]\d|2[0-3])":"[0-5]\d":"[0-5]\d"."\d{3}"+"(0\d|1[0-3])":"[0,3]0
  */
 export type ISO8601 = string
+export function varifyDatetime(str: string): boolean {
+    return /\d+-(0\d|1[0-2])-([0-2]\d|3[0-1])T([0-1]\d|2[0-3]):[0-5]\d:[0-5]\d\.\d{3}\+(0\d|1[0-3]):[0,3]0/.test(str)
+}
 
 export class request {
     forwardFrom: string
@@ -31,10 +37,26 @@ export class request {
         this.timestamp = new Date().toISOString()
         this.data = Data
     }
+
+    static parse(Data: request): request {
+        if (typeof Data.forwardFrom === "undefined") throw "'forwardFrom' undefined"
+        if (!Data.timestamp || !varifyDatetime(Data.timestamp)) throw "'timestamp' not match"
+        return Data
+    }
 }
 
 export class response {
     code: number
     msg: string
     data?: any
+
+    constructor (Msg: string, Code: number = 0, Data: any = undefined) {
+        this.msg = Msg
+        this.code = Code
+        if (typeof Data !== "undefined") this.data = Data
+    }
+
+    public toString(this: response): string {
+        return JSON.stringify(this)
+    }
 }
--- twitkit-app/src/watcher.ts
+++ twitkit-app/src/watcher.ts
@@ -1,9 +1,11 @@
-import { Context } from 'koishi-core'
+import { Context, Logger } from 'koishi-core'
 import * as http from 'http'
 import { parse } from 'url'
 import * as utils from './utils'
 import { Twitter } from './twitter'
 import store from './store'
+
+let logger: Logger
 
 class rss {
     taskId: utils.uuid
@@ -14,12 +16,22 @@ class rss {
     media?: string[]
     author: string
     postDate?: utils.ISO8601
+
+    static parse(Data: rss): rss {
+        if (typeof Data.taskId === "undefined"
+            || typeof Data.content === "undefined"
+            || typeof Data.author === "undefined"
+            || typeof Data.postDate !== "undefined"
+            && !utils.varifyDatetime(Data.postDate)
+        ) throw "param lost"
+        return Data
+    }
 }
 
 function Twitter2msg(tw: Twitter, argv = { ispro: true, prefix: '#' }): string {
     let msg: string = "【" + tw.user.name + "】" + tw.type
-                    + "\n----------------\n"
-                    + "内容：" + tw.content
+        + "\n----------------\n"
+        + "内容：" + tw.content
     if (tw.media) {
         msg += "\n媒体："
         for (const img of tw.media) msg += argv.ispro ? "[CQ:image,file=" + img + "]" : img
@@ -27,8 +39,6 @@ function Twitter2msg(tw: Twitter, argv = { ispro: true, prefix: '#' }): string {
     msg += "\n原链接：" + tw.url + "\n快速嵌字发送：" + argv.prefix + tw.id + " 译文"
     return msg
 }
-
-let Logger
 
 function rss2msg(tw: rss, argv = { ispro: true, prefix: '#' }): string {
     let msg: string = "【" + tw.author + "】"
@@ -40,88 +50,91 @@ function rss2msg(tw: rss, argv = { ispro: true, prefix: '#' }): string {
     }
     if (tw.url) msg += "\n原链接：" + tw.url
     if (tw.tid && tw.tid.length) msg += "\n识别到本条发布包含" + tw.tid.length + "条烤推结果："
-                                    + tw.tid.join(',') + "\n - 发送#-批量隐藏已发推特"
-                                    + "\n- 发送" + argv.prefix + "~核对记录的推文是否全部发布"
-                                    + "\n- 发送" + argv.prefix + "/将快速搜索起始位置更新为即将到来的下一条推特"
-                                    + "\n辛苦了！"
+        + tw.tid.join(', ') + "\n - 发送#-批量隐藏已发推特"
+        + "\n- 发送" + argv.prefix + "~核对记录的推文是否全部发布"
+        + "\n- 发送" + argv.prefix + "/将快速搜索起始位置更新为即将到来的下一条推特"
+        + "\n辛苦了！"
     return msg
 }
 
 function sendmsg(ctx: Context, target: any = { discuss: [], private: [], group: [] }, msg: string, tid: number = null): void {
-    Logger.debug("msg: " + msg)
+    logger.debug("msg: " + msg)
     for (const j of target.discuss) {
         ctx.sender.sendDiscussMsgAsync(j, msg)
-        Logger.debug("tid: %d, target: D_%d send", tid, j)
+        logger.debug("tid: %d, target: D_%d send", tid, j)
     }
     for (const j of target.private) {
         ctx.sender.sendPrivateMsgAsync(j, msg)
-        Logger.debug("tid: %d, target: P_%d send", tid, j)
+        logger.debug("tid: %d, target: P_%d send", tid, j)
     }
     for (const j of target.group) {
         ctx.sender.sendGroupMsgAsync(j, msg)
-        Logger.debug("tid: %d, target: G_%d send", tid, j)
+        logger.debug("tid: %d, target: G_%d send", tid, j)
     }
 }
 
 export default function (ctx: Context, argv: any = { target: {}, ispro: true, port: 1551, prefix: '#' }) {
-    Logger = ctx.logger()
-    Logger.debug("watcher server starting...")
+    logger = ctx.logger("app:watcher")
+    logger.debug("watcher server starting...")
     const server = http.createServer((req, res) => {
         let pathname = parse(req.url).pathname;
-        Logger.debug(req.method + " " + pathname + " HTTP " + req.httpVersion)
-        res.writeHead(200, {'Content-Type': 'application/json'})
+        logger.debug(req.method + " " + pathname + " HTTP " + req.httpVersion)
+        res.writeHead(200, { 'Content-Type': 'application/json' })
         let r = /^\/api\/app\/(twitter|other)$/.exec(pathname)
         if (req.method === "POST" && r) {
             let raw: string = ""
             req.on("data", (chunk) => { raw += chunk })
             req.on("end", async () => {
                 let data: utils.request
+                logger.debug(raw)
                 try {
-                    data = JSON.parse(raw)
-                    Logger.debug("[" + data.forwardFrom + "] " + data.timestamp + " :")
-                    res.end('{"code": 0, "message": "copy"}')
+                    data = utils.request.parse(JSON.parse(raw))
+                    logger.info("[" + data.forwardFrom + "] " + data.timestamp + " :")
+                    res.end(new utils.response("copy").toString())
                 } catch (e) {
-                    Logger.warn("Parse data error: " + e)
-                    res.end('{"code": 400, "message": "Data format error"}')
-                    return //res.end()
+                    logger.warn("Parse data error: " + e)
+                    res.end(new utils.response("Data format error", 400).toString())
+                    return
                 }
+                logger.debug(data.data)
                 switch (r[1]) {
                     case "twitter":
                         for (const i in data.data) if (data.data[i]) {
-                            Logger.debug("Event %s, tid: %d", i, data.data[i])
-                            const tw:Twitter = await store.get(data.data[i])
-                            Logger.debug("[" + tw.user.name + "]" + tw.content)
-                            const msg:string = Twitter2msg(tw, argv)
+                            logger.debug("Event %s, tid: %d", i, data.data[i])
+                            const tw: Twitter = await store.get(data.data[i])
+                            logger.debug("[" + tw.user.name + "]" + tw.content)
+                            const msg: string = Twitter2msg(tw, argv)
                             sendmsg(ctx, argv.target, msg, data.data[i])
                         } else {
-                            Logger.debug("Event %s, no update", i)
+                            logger.debug("Event %s, no update", i)
                         }
-                        Logger.info("Update notice done")
+                        logger.info("Update notice done")
                         break
                     case "other":
                         let rdata: rss
                         try {
-                            rdata = <rss> data.data
-                            Logger.debug("[" + rdata.postDate + "] " + rdata.content)
+                            rdata = rss.parse(data.data)
+                            logger.debug("[" + rdata.postDate + "] " + rdata.content)
                         } catch (e) {
-                            Logger.warn("Parse rss data error: " + e)
+                            logger.warn("Parse rss data error: " + e)
+                            return
                         }
-                        const msg:string = rss2msg(rdata, argv)
+                        const msg: string = rss2msg(rdata, argv)
                         sendmsg(ctx, argv.target, msg)
-                        Logger.info("Update notice done")
+                        logger.info("Update notice done")
                         break
                     default:
                 }
             })
         } else {
-            Logger.warn("Unknow request: " + pathname)
-            res.end('{"code": 404, "message": "Not found"}')
+            logger.warn("Unknow request: " + pathname)
+            res.end(new utils.response("Not Found", 404).toString())
         }
     })
     try {
         server.listen(argv.port);
-        Logger.success("Listening watcher on port %d", argv.port)
+        logger.success("Listening watcher on port %d", argv.port)
     } catch (e) {
-        Logger.warn("Listen watcher fail on port " + argv.port + ": " + e)
+        logger.warn("Listen watcher fail on port " + argv.port + ": " + e)
     }
 }
