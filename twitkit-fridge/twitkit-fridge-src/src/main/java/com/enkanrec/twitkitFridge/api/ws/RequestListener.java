51e9e1409ca07bfab30ae192b64aaf6439d02d95
Rinka <jiajidi@163.com>
2020-02-09 17:03:03 +0800

    Update: support websocket via SocketIO

--- twitkit-fridge/twitkit-fridge-src/src/main/java/com/enkanrec/twitkitFridge/api/ws/RequestListener.java
+++ twitkit-fridge/twitkit-fridge-src/src/main/java/com/enkanrec/twitkitFridge/api/ws/RequestListener.java
@@ -15,6 +15,7 @@ import com.enkanrec.twitkitFridge.api.rest.TaskController;
 import com.enkanrec.twitkitFridge.monitor.WebSocketMonitor;
 import com.enkanrec.twitkitFridge.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import java.lang.reflect.Parameter;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Class : RequestListener
@@ -34,6 +36,8 @@ import java.util.Map;
 @Slf4j
 @Component
 public class RequestListener implements DataListener<String> {
+
+    private static final String LOG_KEY_REQUEST_ID = "requestId";
 
     @Autowired
     private KVConfigController kvConfigController;
@@ -88,51 +92,58 @@ public class RequestListener implements DataListener<String> {
 
     @Override
     public void onData(SocketIOClient client, String data, AckRequest ackSender) throws Exception {
-        long beginTs = System.currentTimeMillis();
-        Map baseForm = JsonUtil.parse(data, Map.class);
-        String useController = (String) baseForm.get("of");
-        String useMethod = (String) baseForm.get("command");
-        log.info(String.format("Client request[%s]: %s %s", client.getSessionId(), useController, useMethod));
-        Object chosenController;
-        switch (useController) {
-            case "kv":
-                chosenController = this.kvConfigController;
-                break;
-            case "task":
-                chosenController = this.taskController;
-                break;
-            default:
-                String hint = "unsupported controller in `of`: " + useController;
+        try {
+            long beginTs = System.currentTimeMillis();
+            Map baseForm = JsonUtil.parse(data, Map.class);
+            String useController = (String) baseForm.get("of");
+            String useMethod = (String) baseForm.get("command");
+            String requestId = UUID.randomUUID().toString();
+            MDC.put(LOG_KEY_REQUEST_ID, requestId);
+            log.info(String.format("Client request[RequestID:%s SessionID:%s]: Controller:%s UriMethod:%s", requestId, client.getSessionId(), useController, useMethod));
+            Object chosenController;
+            switch (useController) {
+                case "kv":
+                    chosenController = this.kvConfigController;
+                    break;
+                case "task":
+                    chosenController = this.taskController;
+                    break;
+                default:
+                    String hint = "unsupported controller in `of`: " + useController;
+                    log.warn(hint);
+                    client.sendEvent(FridgeWSServer.RESPONSE_EVT, StandardResponse.exception(hint));
+                    return;
+            }
+            String methodKey = useController + "&" + useMethod;
+            Method chosenMethod = this.methodAccessMap.get(methodKey);
+            if (chosenMethod != null) {
+                Class chosenForm = this.formClassAccessMap.get(chosenMethod);
+                Object formIns = chosenForm.newInstance();
+                Method parseMethod = JsonDataFridgeForm.class.getMethod("fromRawString", String.class);
+                parseMethod.invoke(formIns, data);
+                try {
+                    Object resp = chosenMethod.invoke(chosenController, formIns);
+                    String handlerName = chosenController.getClass().getSimpleName() + "." + chosenMethod.getName();
+                    log.info(String.format("ws request handled [%s], prepare to emit.", handlerName));
+                    client.sendEvent(FridgeWSServer.RESPONSE_EVT, resp);
+                    String code = String.valueOf(((StandardResponse) resp).getCode());
+                    this.monitor.responseTimeInMs.labels(handlerName, "/" + useMethod, code)
+                            .observe(System.currentTimeMillis() - beginTs);
+                } catch (Exception invEx) {
+                    String hint = "exception at invoke: " + invEx.getMessage();
+                    log.warn(hint);
+                    client.sendEvent(FridgeWSServer.RESPONSE_EVT, StandardResponse.exception(hint));
+                    this.monitor.exceptionCounter.inc();
+                }
+            } else {
+                String hint = "unsupported method: " + methodKey;
                 log.warn(hint);
                 client.sendEvent(FridgeWSServer.RESPONSE_EVT, StandardResponse.exception(hint));
                 return;
-        }
-        String methodKey = useController + "&" + useMethod;
-        Method chosenMethod = this.methodAccessMap.get(methodKey);
-        if (chosenMethod != null) {
-            Class chosenForm = this.formClassAccessMap.get(chosenMethod);
-            Object formIns = chosenForm.newInstance();
-            Method parseMethod = JsonDataFridgeForm.class.getMethod("fromRawString", String.class);
-            parseMethod.invoke(formIns, data);
-            try {
-                Object resp = chosenMethod.invoke(chosenController, formIns);
-                String handlerName = chosenController.getClass().getSimpleName() + "." + chosenMethod.getName();
-                log.info(String.format("ws request handled [%s], prepare to emit.", handlerName));
-                client.sendEvent(FridgeWSServer.RESPONSE_EVT, resp);
-                String code = String.valueOf(((StandardResponse) resp).getCode());
-                this.monitor.responseTimeInMs.labels(handlerName, "/" + useMethod, code)
-                        .observe(System.currentTimeMillis() - beginTs);
-            } catch (Exception invEx) {
-                String hint = "exception at invoke: " + invEx.getMessage();
-                log.warn(hint);
-                client.sendEvent(FridgeWSServer.RESPONSE_EVT, StandardResponse.exception(hint));
-                this.monitor.exceptionCounter.inc();
             }
-        } else {
-            String hint = "unsupported method: " + methodKey;
-            log.warn(hint);
-            client.sendEvent(FridgeWSServer.RESPONSE_EVT, StandardResponse.exception(hint));
-            return;
+        }
+        finally {
+            MDC.remove(LOG_KEY_REQUEST_ID);
         }
     }
 }
