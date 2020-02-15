defcb800bc664f479cec09222cadb5589831f8ba
MisakaHonoka <2361547758@qq.com>
2020-02-15 16:27:01 +0800

    bugfix: remove version check before connect

--- twitkit-app/src/index.ts
+++ twitkit-app/src/index.ts
@@ -8,9 +8,9 @@ export {
     watcher
 }
 
-export async function apply (ctx: Context, argv?: config) {
+export function apply (ctx: Context, argv?: config) {
     const Logger = ctx.logger("info")
-    if (argv.ispro && await ctx.sender.canSendImage() === false) argv.ispro = false
+    // if (argv.ispro && ctx.app.version.coolqEdition !== "pro") argv.ispro = false
     Logger.debug("apply plugin watcher")
     ctx.plugin(watcher, argv)
     Logger.debug("apply plugin cmd")
