27f31d9ffaa9ff7a616f6a8678777454cc79eb23
YYY <yyy3752@gmail.com>
2020-03-16 02:53:14 +0900

    change application.properties; remove db spec from db_test_init.sql

--- twitkit-fridge/twitkit-fridge-src/schema/db_test_init.sql
+++ twitkit-fridge/twitkit-fridge-src/schema/db_test_init.sql
@@ -65,23 +65,23 @@ CREATE TABLE `enkan_twitter` (
   UNIQUE KEY `key_twitteruid` (`twitter_uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `yui_test`.`enkan_config` VALUES (1, '___DEFAULT___', 'test.default.yachiyo.love', 'iroha', '2020-02-07 00:12:56', '2020-02-07 00:12:56');
-INSERT INTO `yui_test`.`enkan_config` VALUES (2, '___DEFAULT___', 'test.default.rika', '五十铃怜', '2020-02-07 00:13:19', '2020-02-07 00:13:19');
-INSERT INTO `yui_test`.`enkan_config` VALUES (3, '___TEST_MB4___', 'test.mb4.emoji', '❤①+123AB', '2020-02-07 00:26:48', '2020-02-07 00:29:46');
+INSERT INTO `enkan_config` VALUES (1, '___DEFAULT___', 'test.default.yachiyo.love', 'iroha', '2020-02-07 00:12:56', '2020-02-07 00:12:56');
+INSERT INTO `enkan_config` VALUES (2, '___DEFAULT___', 'test.default.rika', '五十铃怜', '2020-02-07 00:13:19', '2020-02-07 00:13:19');
+INSERT INTO `enkan_config` VALUES (3, '___TEST_MB4___', 'test.mb4.emoji', '❤①+123AB', '2020-02-07 00:26:48', '2020-02-07 00:29:46');
 
-INSERT INTO `yui_test`.`enkan_task` VALUES (1000, '10000', 'URL_0', '内容0', '[]', 0, 0, '', '2020-02-07 00:02:21', '2020-03-01 12:27:41', '123000123', NULL, NULL, NULL);
-INSERT INTO `yui_test`.`enkan_task` VALUES (1001, '10001', 'URL_1', '内容1', '[]', 0, 0, '', '2020-02-07 00:02:41', '2020-03-01 12:27:42', '123000123', NULL, NULL, NULL);
-INSERT INTO `yui_test`.`enkan_task` VALUES (1002, '10002', 'URL_2', '内容2🍒', '[\"media_2\"]', 1, 0, '', '2020-02-07 16:12:41', '2020-03-01 12:27:43', '123000123', NULL, NULL, NULL);
-INSERT INTO `yui_test`.`enkan_task` VALUES (1003, '10003', 'URL_3', '内容3', '[]', 1, 0, '', '2020-02-07 16:59:03', '2020-03-01 12:27:43', '123000123', NULL, NULL, NULL);
-INSERT INTO `yui_test`.`enkan_task` VALUES (1004, '10004', 'URL_4_HIDED', '内容4', '[]', 0, 1, '', '2020-02-07 17:20:58', '2020-03-01 12:27:46', '123000123', NULL, NULL, NULL);
+INSERT INTO `enkan_task` VALUES (1000, '10000', 'URL_0', '内容0', '[]', 0, 0, '', '2020-02-07 00:02:21', '2020-03-01 12:27:41', '123000123', NULL, NULL, NULL);
+INSERT INTO `enkan_task` VALUES (1001, '10001', 'URL_1', '内容1', '[]', 0, 0, '', '2020-02-07 00:02:41', '2020-03-01 12:27:42', '123000123', NULL, NULL, NULL);
+INSERT INTO `enkan_task` VALUES (1002, '10002', 'URL_2', '内容2🍒', '[\"media_2\"]', 1, 0, '', '2020-02-07 16:12:41', '2020-03-01 12:27:43', '123000123', NULL, NULL, NULL);
+INSERT INTO `enkan_task` VALUES (1003, '10003', 'URL_3', '内容3', '[]', 1, 0, '', '2020-02-07 16:59:03', '2020-03-01 12:27:43', '123000123', NULL, NULL, NULL);
+INSERT INTO `enkan_task` VALUES (1004, '10004', 'URL_4_HIDED', '内容4', '[]', 0, 1, '', '2020-02-07 17:20:58', '2020-03-01 12:27:46', '123000123', NULL, NULL, NULL);
 
-INSERT INTO `yui_test`.`enkan_translate` VALUES (1, 1000, 0, '翻译A1', '[img1]', '2020-02-07 00:07:56', '2020-02-07 00:07:56');
-INSERT INTO `yui_test`.`enkan_translate` VALUES (3, 1000, 1, '翻译A2', '[img2]', '2020-02-07 00:08:18', '2020-02-07 00:08:47');
-INSERT INTO `yui_test`.`enkan_translate` VALUES (4, 1000, 2, '翻译A3', '[img3]', '2020-02-07 00:08:46', '2020-02-07 00:08:46');
-INSERT INTO `yui_test`.`enkan_translate` VALUES (5, 1001, 0, '翻译B1', '[img_21]', '2020-02-07 00:10:30', '2020-02-07 00:10:30');
-INSERT INTO `yui_test`.`enkan_translate` VALUES (6, 1001, 1, '翻译B2', '[img_22]', '2020-02-07 00:10:57', '2020-02-07 00:10:57');
-INSERT INTO `yui_test`.`enkan_translate` VALUES (8, 1002, 0, '翻译C1', '[img_31]', '2020-02-07 17:21:34', '2020-02-07 17:21:34');
+INSERT INTO `enkan_translate` VALUES (1, 1000, 0, '翻译A1', '[img1]', '2020-02-07 00:07:56', '2020-02-07 00:07:56');
+INSERT INTO `enkan_translate` VALUES (3, 1000, 1, '翻译A2', '[img2]', '2020-02-07 00:08:18', '2020-02-07 00:08:47');
+INSERT INTO `enkan_translate` VALUES (4, 1000, 2, '翻译A3', '[img3]', '2020-02-07 00:08:46', '2020-02-07 00:08:46');
+INSERT INTO `enkan_translate` VALUES (5, 1001, 0, '翻译B1', '[img_21]', '2020-02-07 00:10:30', '2020-02-07 00:10:30');
+INSERT INTO `enkan_translate` VALUES (6, 1001, 1, '翻译B2', '[img_22]', '2020-02-07 00:10:57', '2020-02-07 00:10:57');
+INSERT INTO `enkan_translate` VALUES (8, 1002, 0, '翻译C1', '[img_31]', '2020-02-07 17:21:34', '2020-02-07 17:21:34');
 
-INSERT INTO `yui_test`.`enkan_twitter` VALUES (4, '123000123', 'enkanRecGLZ', '圆环纪录攻略组', 'http://111/111.jpg', '2020-03-01 12:42:25', '2020-03-01 12:08:29');
+INSERT INTO `enkan_twitter` VALUES (4, '123000123', 'enkanRecGLZ', '圆环纪录攻略组', 'http://111/111.jpg', '2020-03-01 12:42:25', '2020-03-01 12:08:29');
 
 COMMIT;
