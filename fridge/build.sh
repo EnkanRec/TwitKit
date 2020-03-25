e375e618d4019ee344b0222b0a408116200a48c0
inactive-virus <gigagerm@hotmail.com>
2020-03-25 10:20:18 +0800

    rearrange Fridge (README not yet)

--- fridge/build.sh
+++ fridge/build.sh
@@ -1,6 +1,5 @@
+#!/bin/bash -e
 basepath=$(cd `dirname $0`; pwd)
-git checkout dev
-git pull
-cd "${basepath}/../twitkit-fridge-src"
+cd "${basepath}/fridge-src"
 mvn clean
 mvn package #-Dmaven.test.skip=true
--- fridge/start.sh
+++ fridge/start.sh
@@ -1,3 +1,3 @@
+#!/bin/bash -e
 basepath=$(cd `dirname $0`; pwd)
-cd "${basepath}/.."
-nohup java -jar "${basepath}/../twitkit-fridge-src/target/twitkit-fridge-0.0.1-SNAPSHOT.jar" --spring.profiles.active=prod > std.output.log 2>&1& echo $! > "${basepath}/../run.pid"
+java -jar "${basepath}/fridge-src/target/twitkit-fridge-0.0.1-SNAPSHOT.jar"
