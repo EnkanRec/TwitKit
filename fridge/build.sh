39275620814bf2354c49b34d6c631e5893db1095
inactive-virus <gigagerm@hotmail.com>
2020-03-25 12:06:24 +0800

    Rearrange fridge (Readme not yet)

--- fridge/build.sh
+++ fridge/build.sh
@@ -1,6 +1,5 @@
+#!/bin/bash -e
 basepath=$(cd `dirname $0`; pwd)
-git checkout dev
-git pull
-cd "${basepath}/../twitkit-fridge-src"
+cd "${basepath}/twitkit-fridge-src"
 mvn clean
 mvn package #-Dmaven.test.skip=true
